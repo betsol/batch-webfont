c96d00b31beb5ef553b45eaba079968d5cc94824
Slava Fomin II <s.fomin@betsol.ru>
2015-12-16 14:05:35 +0300

    Some fixes for site.

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var runSequence = require('run-sequence');
 var compass = require('gulp-compass');
 var rename = require('gulp-rename');
 var minifyCSS = require('gulp-minify-css');
+var serverFactory = require('spa-server');
+var deploy = require('gulp-gh-pages');
 
 //---------------//
 // CONFIGURATION //
@@ -16,6 +18,9 @@ var minifyCSS = require('gulp-minify-css');
 var cssDir = './css';
 var scssDir = './scss';
 var miniSuffix = '.min';
+var deployTmpPath = './.deploy';
+
+var demoServer;
 
 //-------//
 // TASKS //
@@ -55,3 +60,93 @@ gulp.task('build:styles', function() {
     .pipe(gulp.dest(cssDir))
   ;
 });
+
+
+//===========//
+// WEBSERVER //
+//===========//
+
+gulp.task('webserver.start', function (callback) {
+  demoServer = serverFactory.create({
+    path: './site',
+    port: 8888,
+    serveStaticConfig: {
+      index: 'index.html'
+    }
+  });
+  demoServer.start(callback);
+});
+
+gulp.task('webserver.stop', function (callback) {
+  demoServer.stop(callback);
+});
+
+
+//======//
+// SITE //
+//======//
+
+gulp.task('site', ['webserver.start']);
+
+gulp.task('site-deploy', function (done) {
+  runSequence(
+    'site-deploy-before',
+    'site-deploy-actual',
+    'site-deploy-after',
+    done
+  );
+});
+
+gulp.task('site-deploy-actual', function () {
+
+  console.log('Starting to deploy files...');
+
+  return gulp.src(deployTmpPath + '/**/*')
+    .pipe(deploy())
+  ;
+
+});
+
+gulp.task('site-deploy-before', function (done) {
+
+  // Clearing temp directories and making a temp copy.
+  deployClearTemp(function () {
+    makeTempCopy(done);
+  });
+
+
+  /**
+   * Makes a temporary copy of the site directory with symlinks resolved.
+   *
+   * @param {function} callback
+   */
+  function makeTempCopy (callback) {
+    ncp('./site', deployTmpPath, {
+      dereference: true
+    }, function (error) {
+      if (error) {
+        return console.error(error);
+      }
+      console.log('Temporary copy created!');
+      callback();
+    });
+  }
+
+});
+
+gulp.task('site-deploy-after', function (done) {
+  deployClearTemp(done);
+});
+
+
+/**
+ * Clears temp directory.
+ *
+ * @param {function} callback
+ */
+function deployClearTemp (callback) {
+  del([deployTmpPath, './.publish'], function () {
+    console.log('Temporary directories removed!');
+    callback();
+  });
+}
